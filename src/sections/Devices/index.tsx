f82d5bf60b9a5783ff3122988b94eebf888eb707
salomaosilval <salomaosilval@hotmail.com>
2023-05-04 19:30:14 -0400

    :sparkles: feat: add Devices section

---
+++ src/sections/Devices/index.tsx
@@ -0,0 +1,23 @@
+import "./styles.scss";
+
+export const Devices = () => {
+  return (
+    <>
+      <div className="devicesContainer">
+        <img src="https://www-growth.scdn.co/static/download/all-devices.svg" alt="All devices" />
+        <h1>Uma só conta para curtir a música em qualquer dispositivo.</h1>
+        <div className="devices">
+          <p>Celular</p>
+          <p>Computador</p>
+          <p>Tablet</p>
+          <p>Carro</p>
+          <p>Playstation</p>
+          <p>Xbox</p>
+          <p>TV</p>
+          <p>Alto-falante</p>
+          <p>Web Player</p>
+        </div>
+      </div>
+    </>
+  );
+};
